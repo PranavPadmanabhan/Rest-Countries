11dfd9e5403093c43ed0484e14ffb616251f70ee
PranavPadmanabhan <PranavPadmanabhan@gmail.com>
2022-01-26 11:42:03 +0530

    api integrated

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
-import { useContext, useEffect, useState } from 'react';
-import { FaChevronDown, FaSearch } from 'react-icons/fa';
+import React from 'react';
 import './App.css';
-import Country from './components/navbar/Country/Country';
-import Navbar from './components/navbar/navbar';
 import ThemeContextProvider from './constants/ThemeContext';
-import { ThemeContext } from './constants/ThemeContext'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Home from './screens/Home/Home';
 
 function App() {
 
-  const [dropdown, setDropdown] = useState(false);
-  const [selectedRegion, setSelectedRegion] = useState("Filter by Region");
-  const [regions, setregions] = useState(["Africa","America","Asia","Europe","Oceania"]);
-
+  
   return (
-    <ThemeContextProvider>
-      <ThemeContext.Consumer >
-        {
-          ({ SelectTheme, DarkTheme, preferredTheme }) => (
-            <div className="App" style={{ backgroundColor: preferredTheme.backgroundColor }}>
-              <Navbar />
-              <div className="short-navbar" style={{ backgroundColor: preferredTheme.backgroundColor }}>
-                <div className="search" style={{backgroundColor:preferredTheme.elementColor}}>
-                  <FaSearch color={DarkTheme?"grey":"black"}/>
-                  <input placeholder='Search' type="text" className="Input" style={{backgroundColor:preferredTheme.elementColor}}/>
-                </div>
-                <div className="dropdown" style={{backgroundColor:preferredTheme.elementColor}}>
-                  <div onClick={() => setDropdown(!dropdown)} className="dropdown-title-box" style={{backgroundColor:preferredTheme.elementColor}}>
-                    <span onClick={() => setDropdown(!dropdown)} className="dropdown-title" style={{color:preferredTheme.textColor}}>{selectedRegion}</span>
-                    <FaChevronDown onClick={() => setDropdown(!dropdown)} color={DarkTheme?"white":"black"}/>
-                  </div>
-                  {dropdown && (
-                    <div className="dropdown-menu">
-                      {
-                        regions.map((item) => (
-                          <span 
-                          onClick={() => {
-                            setSelectedRegion(item);
-                            setDropdown(false)
-                          }} 
-                          className="dropdown-items" 
-                          style={{backgroundColor:preferredTheme.elementColor,color:preferredTheme.textColor}}>{item}</span>
-
-                        ))
-                      }
-                  </div>
-                  )}
-                </div>
-              </div>
-              <div className="grid">
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-                <Country />
-              </div>
-            </div>
-          )
-        }
-      </ThemeContext.Consumer>
-    </ThemeContextProvider>
+    <Router>
+      <ThemeContextProvider>
+        <Routes >
+          <Route exact path={"/"} element={<Home />} />
+        </Routes>
+      </ThemeContextProvider>
+    </Router>
   );
 }
 
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,157 @@
+import { useContext, useEffect, useState } from 'react';
+import { FaChevronDown, FaSearch } from 'react-icons/fa';
+import './Home.css';
+import axios from 'axios';
+import Navbar from '../../components/navbar/navbar';
+import Country from '../../components/navbar/Country/Country';
+import { ThemeContext } from '../../constants/ThemeContext';
+
+function Home() {
+
+    const [dropdown, setDropdown] = useState(false);
+    const [selectedRegion, setSelectedRegion] = useState("Filter by Region");
+    const [regions, setregions] = useState([
+        {
+            title: "All",
+            endpoint: "all"
+        },
+        {
+            title: "European Union",
+            endpoint: "eu"
+        },
+        {
+            title: "European Free Trade Association",
+            endpoint: "efta"
+        },
+        {
+            title: "Caribbean Community",
+            endpoint: "caricom"
+        },
+        {
+            title: "Pacific Alliance",
+            endpoint: 'pa'
+        },
+        {
+            title: "African Union",
+            endpoint: "au"
+        },
+        {
+            title: "Union of South American Nations",
+            endpoint: "usan"
+        },
+        {
+            title: "Eurasian Economic Union",
+            endpoint: "eeu"
+        },
+        {
+            title: "Arab League",
+            endpoint: 'au'
+        },
+        {
+            title: "Association of Southeast Asian Nations",
+            endpoint: "asean"
+        },
+        {
+            title: "Central American Integration System",
+            endpoint: "cais"
+        },
+        {
+            title: "Central European Free Trade Agreement",
+            endpoint: "cefta"
+        },
+        {
+            title: "North American Free Trade Agreement",
+            endpoint: "nafta"
+        },
+        {
+            title: "South Asian Association for Regional Cooperation",
+            endpoint: "saarc"
+        }
+    ]);
+    const [countries, setCountries] = useState([]);
+    const [keyword, setKeyword] = useState("");
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchCountries('all')
+    }, []);
+
+    useEffect(() => {
+        setCountries(data.filter((item) => item.name.toLowerCase().includes(keyword)));
+    }, [keyword]);
+
+
+    const fetchCountries = (Endpoint) => {
+        var url = `https://restcountries.com/v2/${Endpoint}`
+        axios.get(url).then((res) => {
+            setCountries(res.data);
+            if (Endpoint == 'all') {
+                setData(res.data)
+            }
+        })
+    }
+
+
+    return (
+        <ThemeContext.Consumer>
+            {
+                ({preferredTheme, DarkTheme})=> (
+                    <div className="App" style={{ backgroundColor: preferredTheme.backgroundColor }}>
+            <Navbar />
+            <div className="short-navbar" style={{ backgroundColor: preferredTheme.backgroundColor }}>
+                <div className="search" style={{ backgroundColor: preferredTheme.elementColor }}>
+                    <FaSearch color={DarkTheme ? "grey" : "black"} />
+                    <input onChange={(e) => {
+                        setKeyword(e.target.value)
+                    }}
+                        placeholder='Search'
+                        type="text"
+                        className="Input"
+                        style={{ backgroundColor: preferredTheme.elementColor }} />
+                </div>
+                <div className="dropdown" style={{ backgroundColor: preferredTheme.elementColor }}>
+                    <div onClick={() => setDropdown(!dropdown)} className="dropdown-title-box" style={{ backgroundColor: preferredTheme.elementColor }}>
+                        <span onClick={() => setDropdown(!dropdown)} className="dropdown-title" style={{ color: preferredTheme.textColor }}>{selectedRegion}</span>
+                        <FaChevronDown onClick={() => setDropdown(!dropdown)} color={DarkTheme ? "white" : "black"} />
+                    </div>
+                    {dropdown && (
+                        <div className="dropdown-menu">
+                            {
+                                regions.map((item) => (
+                                    <span
+                                        onClick={() => {
+                                            setSelectedRegion(item.title);
+                                            fetchCountries(item.title == 'All' ? 'all' : `regionalbloc/${item.endpoint}`)
+                                            setDropdown(false)
+                                        }}
+                                        className="dropdown-items"
+                                        style={{ backgroundColor: preferredTheme.elementColor, color: preferredTheme.textColor }}>{item.title}</span>
+
+                                ))
+                            }
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div className="grid">
+                {
+                    countries.map((country, index) => (
+                        <Country
+                            key={index}
+                            name={country.name}
+                            capital={country.capital}
+                            population={country.population}
+                            region={country.region}
+                            url={country.flag}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+                )
+            }
+        </ThemeContext.Consumer>
+    );
+}
+
+export default Home;
