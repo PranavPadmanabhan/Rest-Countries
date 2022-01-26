5f71727dbfbce4fe04f077660adcd27ce265bc4c
PranavPadmanabhan <PranavPadmanabhan@gmail.com>
2022-01-26 19:37:03 +0530

    added to gh pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <ThemeContextProvider>
         <Routes location={"/"}>
           <Route exact path={"/"} element={<Home />} />
