9919688e7ed253d767bcf6b6348059a323da9ef8
PranavPadmanabhan <PranavPadmanabhan@gmail.com>
2022-03-16 11:24:12 +0530

    modified country.js

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom'
 function Country({name,population,region,capital,url,onClick}) {
     const { preferredTheme } = useContext(ThemeContext)
     return (
-        <div onClick={onClick}  className='country' style={{backgroundColor:window.innerWidth<600?"transparent":preferredTheme.elementColor}}>
+        <div onClick={onClick}  className='country' style={{backgroundColor:preferredTheme.headerColor}}>
            <img src={url} alt="" className="flag" />
-           <div className="details">
+           <div className="details" >
                <span className="country-name" style={{color:preferredTheme.textColor}}>{name}</span>
                <span className="population" style={{color:preferredTheme.textColor,fontWeight:"bold"}}>Population  :  &nbsp; {(<span style={{color:preferredTheme.textColor,fontWeight:"400"}} >{population}</span>)}</span>
                <span className="Region" style={{color:preferredTheme.textColor,fontWeight:"bold"}}>Region : &nbsp; {(<span style={{color:preferredTheme.textColor,fontWeight:"400"}} >  {region}</span>)}</span>
