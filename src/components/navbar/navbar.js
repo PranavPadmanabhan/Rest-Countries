fdbab9fc17417d649b2e797349188bd8fa96d151
PranavPadmanabhan <PranavPadmanabhan@gmail.com>
2022-03-16 11:19:27 +0530

    modified darkmode button

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,22 +1,26 @@
 import React, { useContext, useEffect } from 'react';
 import './navbar.css';
-import {FaMoon} from 'react-icons/fa'
-import {ThemeContext} from '../../constants/ThemeContext'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { ThemeContext } from '../../constants/ThemeContext'
 
 function Navbar() {
-  const { SelectTheme, DarkTheme,preferredTheme} = useContext(ThemeContext)
+  const { SelectTheme, DarkTheme, preferredTheme } = useContext(ThemeContext)
   useEffect(() => {
-    
+
   }, []);
-  
+
   return (
-  <div className='header' style={{backgroundColor:preferredTheme.headerColor}} > 
-    <span className="header-title" style={{color:preferredTheme.textColor}}>Where in the world?</span>
-    <div onClick={() => SelectTheme(!DarkTheme)} className="mode-button">
-        <FaMoon style={{marginRight:5}} onClick={() => SelectTheme(!DarkTheme)} color={DarkTheme?"white":"black"} size={18}/>
-        <span style={{color:preferredTheme.textColor}} onClick={() => SelectTheme(!DarkTheme)} className="button-text">Light mode</span>
+    <div className='header' style={{ backgroundColor: preferredTheme.headerColor }} >
+      <span className="header-title" style={{ color: preferredTheme.textColor }}>Where in the world?</span>
+      <div onClick={() => SelectTheme(!DarkTheme)} className="mode-button">
+        {
+          DarkTheme ? (<FaSun style={{ marginRight: 5 }} onClick={() => SelectTheme(!DarkTheme)} color={"white"} size={18} />
+          ) : (<FaMoon style={{ marginRight: 5 }} onClick={() => SelectTheme(!DarkTheme)} color={"black"} size={18} />
+          )
+        }
+        <span style={{ color: preferredTheme.textColor }} onClick={() => SelectTheme(!DarkTheme)} className="button-text">{DarkTheme ? "Light mode" : "Dark mode"}</span>
+      </div>
     </div>
-  </div>
   );
 }
 
